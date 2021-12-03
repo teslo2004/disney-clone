00d7f8eca249aa39dc60834218e767f062bd5621
teslo2004 <teslo2004@gmail.com>
2021-12-03 12:11:17 +0300

    add Navbar menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,81 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/user.svg" />
+    </Nav>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`
+const Nav = styled.div`
   height: 70px;
   background: #000b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  cursor: pointer;
+  align-itemms: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding 0 12px;
+
+    img { 
+        height: 20px;
+    }
+
+    span{
+        font-size: 13px;
+        letter-spacing: 1.42px;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  height: 40px;
+  width: 40px;
+  background-color: white;
+  border-radius: 50%;
+  cursor: pointer;
 `;
